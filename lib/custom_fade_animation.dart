ca89e24c9c6b4b4e9b20f098dafe4486af958845
Jaydip Tank <95556824+Jaydip04@users.noreply.github.com>
2024-08-23 11:26:41 +0530

    add animation

--- lib/custom_fade_animation.dart
+++ lib/custom_fade_animation.dart
@@ -1,7 +1,65 @@
 library custom_fade_animation;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class FadeTransitionExample extends StatefulWidget {
+  const FadeTransitionExample({
+    required this.duration,
+    required this.curve,
+    super.key,
+  });
+
+  final Duration duration;
+
+  final Curve curve;
+
+  @override
+  State<FadeTransitionExample> createState() => _FadeTransitionExampleState();
+}
+class _FadeTransitionExampleState extends State<FadeTransitionExample>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: widget.duration,
+    vsync: this,
+  )..repeat(reverse: true);
+  late final CurvedAnimation _animation = CurvedAnimation(
+    parent: _controller,
+    curve: widget.curve,
+  );
+
+  @override
+  void didUpdateWidget(FadeTransitionExample oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.duration != widget.duration) {
+      _controller
+        ..duration = widget.duration
+        ..repeat(reverse: true);
+    }
+
+    if (oldWidget.curve != widget.curve) {
+      _animation.curve = widget.curve;
+    }
+  }
+
+  @override
+  void dispose() {
+    _animation.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: Colors.white,
+      child: FadeTransition(
+        opacity: _animation,
+        child: const Padding(
+          padding: EdgeInsets.all(8),
+          child: FlutterLogo(),
+        ),
+      ),
+    );
+  }
 }
